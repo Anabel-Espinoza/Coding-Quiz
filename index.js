e17b218b679d7bb250fa48aa85f7467b67370cf5
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-02-10 13:29:02 -0500

    Added timer function

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ let correctCounter = 0
 let wrongCounter = 0
 let startBtn = document.querySelector('#startEl')
 let questionText = document.querySelector('.questionEl')
-let answersText = document.querySelector('.answersText')
 let instructionsText = document.querySelector('.instructionsEl')
 let listAnswers = document.querySelector('.listAnswersEl')
 let answerA = document.querySelector ('#answerAEl')
 let answerB = document.querySelector ('#answerBEl')
 let answerC = document.querySelector ('#answerCEl')
 let answerD = document.querySelector ('#answerDEl')
-
-let answerMessage = document.createElement('p')
-
+let highScoresEl = document.querySelector('.HighScores')
 let questionIndex = 0
 let rightResult = document.querySelector('.correct-message')
 let wrongResult = document.querySelector('.wrong-message')
-let secondsLeft= 15;
+let secondsLeft= 75;
 let timerEl = document.querySelector('.timer-El')
+let saveInfoEl = document.querySelector('.save-info')
+let submitBtn = document.querySelector('#submit-btn')
+let initialsEl = document.querySelector('#initials-el')
 
-
+// questions bank
 const questions = [{
     question: '1. Commonly used data types do NOT include:',
     answers: ['alerts', 'strings','booleans','numbers'],
@@ -36,31 +36,34 @@ const questions = [{
     question: '4. String values must be enclosed within __________ when being assigned to variables.',
     answers: ['commas', 'curly brackets','quotes','parenthesis'],
     correctAnswer: 'quotes'
-} , {
-    question: '5. A very useful tool used during development and debugging for printing content to the debugger is:',
-    answers: ['JavaScript', 'terminal/bash','for loops','console.log'],
-    correctAnswer: 'console.log'
-} , {
-    question: '6. Inside the HTML document, the JavaScript code must be placed in the _________ element',
-    answers: ['<head>', '<footer>','<script>','<link>'],
-    correctAnswer: '<script>'
-}  , {
-    question: '7. For the following array ["a","b","c","d"], the index for letter "a" is:',
-    answers: ['0', '1','2','3'],
-    correctAnswer: '0'
-}  , {
-    question: "8. The method used to remove the last element of my array is:",
-    answers: ['push()', 'unshift()','concat()','pop()'],
-    correctAnswer: 'pop()'
+// } , {
+//     question: '5. A very useful tool used during development and debugging for printing content to the debugger is:',
+//     answers: ['JavaScript', 'terminal/bash','for loops','console.log'],
+//     correctAnswer: 'console.log'
+// } , {
+//     question: '6. Inside the HTML document, the JavaScript code must be placed in the _________ element',
+//     answers: ['<head>', '<footer>','<script>','<link>'],
+//     correctAnswer: '<script>'
+// }  , {
+//     question: '7. For the following array ["a","b","c","d"], the index for letter "a" is:',
+//     answers: ['0', '1','2','3'],
+//     correctAnswer: '0'
+// }  , {
+//     question: "8. The method used to remove the last element of my array is:",
+//     answers: ['push()', 'unshift()','concat()','pop()'],
+//     correctAnswer: 'pop()'
 } ]
 
 startBtn.addEventListener('click', startGame)
 // console.log(questions)
 
+highScoresEl.addEventListener('click', highScores)
+
 function startGame() {
     instructionsText.textContent=" "
     startBtn.style.display = 'none'
     questionIndex=0
+    startTimer()
     startQuestion()
 
     // let timer = setInterval(function() {
@@ -70,21 +73,24 @@ function startGame() {
     //     startQuestion()}, 1000)  
 }
 
-function stopGame() {
-    questionText.textContent = 'All done!'
-    instructionsText.textContent='Your final score is' + correctCounter
+function startTimer() {
+    let timerInterval= setInterval(function () {
+        timerEl.textContent = 'Time: ' + secondsLeft
+        secondsLeft--
+        if (secondsLeft <= 0 || questionIndex===questions.length) {
+            clearInterval(timerInterval);
+            timerEl.textContent = 'Time: ' + secondsLeft
+            stopGame()
+        }
+    }, 1000);
 }
 
 function startQuestion() {
-    
-        //set question, answer options and add format 
         // console.log(questionIndex)
         questionText.textContent = questions[questionIndex].question
-        // debugger
         listAnswers.setAttribute('style','display:flex; flex-direction:column; justify-content:center; margin-top: 30px')
         // console.log(listAnswers)
         for (let i=0; i<4; i++) {
-            // debugger
             listAnswers.children[i].innerText = questions[questionIndex].answers[i]
             listAnswers.children[i].setAttribute('style', 'display:block; margin:0.2rem; max-width:300px; padding: 0.5rem')
 
@@ -92,10 +98,9 @@ function startQuestion() {
             event.preventDefault()
             // console.log(questions[questionIndex].question)
 
-            console.log('correct', questions[questionIndex].correctAnswer)
+            // console.log('correct', questions[questionIndex].correctAnswer)
             console.log('selected', listAnswers.children[i].innerText)
             if (listAnswers.children[i].innerText===questions[questionIndex].correctAnswer) {
-                // debugger
                 correctCounter++
                 rightResult.style.display= 'block'
                 wrongResult.style.display= 'none'
@@ -103,20 +108,63 @@ function startQuestion() {
                 questionIndex++
 
             } else {
-                // debugger
                 wrongCounter++
+                secondsLeft-=10
                 rightResult.style.display= 'none'
                 wrongResult.style.display= 'block'
                 console.log('wrong')
                 questionIndex++
 
             }   
-            questionText.textContent = questions[questionIndex].question
-            console.log(questionIndex)
-            for (let k=0; k<4; k++)
-            listAnswers.children[k].innerText = questions[questionIndex].answers[k]
-            console.log(questions[questionIndex].answers)
-            })
 
+            //Next question until all questions have been answered
+            if (questionIndex<questions.length) {
+                questionText.textContent = questions[questionIndex].question
+                console.log(questionIndex)
+                for (let j=0; j<4; j++) {
+                    listAnswers.children[j].innerText = questions[questionIndex].answers[j]
+                    console.log(questions[questionIndex].answers)
+                }
+            } else {
+                // clearInterval(timerInterval);
+                // timerEl.textContent = 'Time: ';
+                stopGame()
+            }
+            })
 }  }              
 
+function stopGame() {
+    questionText.textContent = 'All done!'
+    instructionsText.textContent='Your final score is: ' + correctCounter
+    for (let k=0; k<4; k++) {
+        listAnswers.children[k].setAttribute('style', 'display: none')
+    }
+    saveInfoEl.style.display= 'block'
+    console.log(submitBtn)
+    submitBtn.addEventListener('click', highScores)
+}
+
+function highScores() {
+    questionText.textContent = 'High Scores'
+    rightResult.style.display= 'none'
+    wrongResult.style.display= 'none'
+    saveInfoEl.style.display= 'none'
+    instructionsText.textContent=""
+
+    let goBackBtn = document.createElement('button')
+    goBackBtn.textContent = 'Go Back'
+    questionText.appendChild(goBackBtn)
+
+    let ClearScoresBtn = document.createElement('button')
+    ClearScoresBtn.textContent = 'Clear high scores'
+    questionText.appendChild(ClearScoresBtn)
+
+    ClearScoresBtn.style.display = 'block'
+    goBackBtn.style.display = 'block'
+
+    goBackBtn.addEventListener('click', function(){
+        window.location.reload()
+    })
+
+ 
+}
